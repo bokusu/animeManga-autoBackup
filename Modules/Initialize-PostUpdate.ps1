63edc41db22558565b738d7848ff747a3d2f1e9e
Natsu Tadama <nattadasu@nattadasu.my.id>
2022-10-21 16:23:06 +0700

    Resolve env does not load properly

--- Modules/Initialize-PostUpdate.ps1
+++ Modules/Initialize-PostUpdate.ps1
@@ -18,7 +18,7 @@ Function Update-Backupper {
     # Replace update frequency
     Write-Verbose -Message "[$(Get-Date)] Replace backup frequency"
     If ($Env:BACKUP_FREQ) {
-        $loadYaml -Replace 'cron: 0 0 * * SUN', "cron: `"$($Env:BACKUP_FREQ)`""
+        $loadYaml = $loadYaml -Replace 'cron: 0 0 * * SUN', "cron: `"$($Env:BACKUP_FREQ)`""
         $loadYaml | Out-File -FilePath $path -Encoding utf8 -Force
         Write-Verbose -Message "[$(Get-Date)] Backup frequency updated"
     }
@@ -34,7 +34,7 @@ Function Update-Updater {
     # Replace update frequency
     Write-Verbose -Message "[$(Get-Date)] Replace Update frequency"
     If ($Env:UPDATE_FREQ) {
-        $loadYaml -Replace 'cron: "0 0 * * *"', "cron: `"$($Env:UPDATE_FREQ)`""
+        $loadYaml = $loadYaml -Replace 'cron: "0 0 * * *"', "cron: `"$($Env:UPDATE_FREQ)`""
         $loadYaml | Out-File -FilePath $path -Encoding utf8 -Force
         Write-Verbose -Message "[$(Get-Date)] Update frequency updated"
     }
@@ -42,6 +42,18 @@ Function Update-Updater {
         Write-Verbose -Message "[$(Get-Date)] Update frequency not updated"
     }
 }
+
+<#
+Function Remove-RepoTrash {
+    $filePath = @(
+        "./.github/dependabot.yml",
+        "./Modules/Get-AniDBBackup.py"
+    )
+    ForEach ($file in $filePath) {
+        Write-Verbose "[$(Get-Date)] Removing $($file)"
+        Remove-Item -Path $file
+    }
+} #>
 
 Update-Backupper
 Update-Updater
